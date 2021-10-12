43656478396184cc8a64b4056f294e58dc48f380
zergioramos <86982823+zergioramos@users.noreply.github.com>
2021-10-12 17:44:11 -0500

    Actualizacion del Home, se add el body

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,10 +11,10 @@
       
     >
       <v-card-title class="teal">
-        <h6 class="
+        <h5 class="
         font-text Italic text
-        text-center"       
-        >Te esperamos en nuestras redes sociales</h6>
+        text-center"
+        >Te esperamos en nuestras redes sociales</h5>
 
         <v-spacer></v-spacer>
 
